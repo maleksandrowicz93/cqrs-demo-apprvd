42721cb73956e8348f2fffcaddace126b7730f8d
maleksandrowicz93 <mateusz.aleksandrowicz93@gmail.com>
2023-02-08 07:53:51 +0100

    complete tests

--- src/test/groovy/com/github/maleksandrowicz93/cqrsdemo/StudentFacadeTest.groovy
+++ src/test/groovy/com/github/maleksandrowicz93/cqrsdemo/StudentFacadeTest.groovy
@@ -1,9 +1,7 @@
 package com.github.maleksandrowicz93.cqrsdemo
 
-import com.github.maleksandrowicz93.cqrsdemo.user.AddStudentCommand
-import com.github.maleksandrowicz93.cqrsdemo.user.EditStudentCommand
-import com.github.maleksandrowicz93.cqrsdemo.user.StudentDto
 import com.github.maleksandrowicz93.cqrsdemo.user.StudentFacade
+import com.github.maleksandrowicz93.cqrsdemo.user.dto.StudentDto
 import spock.lang.Specification
 
 class StudentFacadeTest extends Specification {
@@ -12,9 +10,9 @@ class StudentFacadeTest extends Specification {
 
     def "get all students"() {
         given: "2 students exist in db"
-        facade.addStudent(new AddStudentCommand())
-        facade.addStudent(new AddStudentCommand())
-        def expectedStudents = List.of(new StudentDto(), new StudentDto())
+        facade.addStudent(StudentUtils.addStudentCommand())
+        facade.addStudent(StudentUtils.alternativeAddStudentCommand())
+        def expectedStudents = List.of(StudentUtils.studentDto(), StudentUtils.alternativeStudentDto())
 
         when: "user tries to retrieve them"
         def students = facade.getAllStudents()
@@ -26,10 +24,10 @@ class StudentFacadeTest extends Specification {
 
     def "add new student"() {
         given: "there is a completely new student data to be added to db"
-        def expectedStudent = new StudentDto()
+        def expectedStudent = StudentUtils.studentDto()
 
         when: "user tries to add a new student"
-        def student = facade.addStudent(new AddStudentCommand())
+        def student = facade.addStudent(StudentUtils.addStudentCommand())
 
         then: "this student should be successfully added"
         student == expectedStudent
@@ -37,23 +35,23 @@ class StudentFacadeTest extends Specification {
 
     def "get student"() {
         given: "a student exists in db"
-        facade.addStudent(new AddStudentCommand())
-        def expectedStudent = new StudentDto()
+        facade.addStudent(StudentUtils.addStudentCommand())
+        def expectedStudent = StudentUtils.studentDto()
 
         when: "user tries to retrieve student's data by student's id"
-        def student = facade.getStudent("")
+        def student = facade.getStudent(StudentUtils.ID)
 
         then: "gets his correct data"
         student == expectedStudent
     }
 
-    def "update student"() {
+    def "edit student"() {
         given: "a student exists in db"
-        facade.addStudent(new AddStudentCommand())
-        def expectedStudent = new StudentDto()
+        facade.addStudent(StudentUtils.addStudentCommand())
+        def expectedStudent = StudentUtils.studentDto()
 
         when: "user tries to edit student's data"
-        def student = facade.editStudentData("", new EditStudentCommand())
+        def student = facade.editStudentData(StudentUtils.ID, StudentUtils.editStudentDataCommand())
 
         then: "data is successfully edited"
         student == expectedStudent
@@ -61,10 +59,10 @@ class StudentFacadeTest extends Specification {
 
     def "update password"() {
         given: "a student exists in db"
-        facade.addStudent(new AddStudentCommand())
+        facade.addStudent(StudentUtils.addStudentCommand())
 
         when: "user tries to update student's password"
-        def updated = facade.updatePassword("", "")
+        def updated = facade.updatePassword(StudentUtils.ID, StudentUtils.PASSWORD)
 
         then: "password is successfully updated"
         updated
@@ -72,10 +70,10 @@ class StudentFacadeTest extends Specification {
 
     def "delete student"() {
         given: "a student exists in db"
-        facade.addStudent(new AddStudentCommand())
+        facade.addStudent(StudentUtils.addStudentCommand())
 
         when: "user tries to delete student's account"
-        def deleted = facade.deleteStudent("")
+        def deleted = facade.deleteStudent(StudentUtils.ID)
 
         then: "account is successfully deleted"
         deleted
