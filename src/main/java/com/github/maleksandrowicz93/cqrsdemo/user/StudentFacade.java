8bfddd34846b7104a7e02844ea06357c14166abb
Mateusz Aleksandrowicz <mateusz.aleksandrowicz93@gmail.com>
2023-02-08 07:38:13 +0100

    rename facade

---
+++ src/main/java/com/github/maleksandrowicz93/cqrsdemo/user/StudentFacade.java
@@ -0,0 +1,32 @@
+package com.github.maleksandrowicz93.cqrsdemo.user;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Component
+@RequiredArgsConstructor
+public class StudentFacade {
+
+    public List<StudentDto> getAllStudents() {
+        return new ArrayList<>();
+    }
+
+    public StudentDto addStudent(AddStudentCommand command) {
+        return new StudentDto();
+    }
+
+    public StudentDto getStudent(String studentId) {
+        return new StudentDto();
+    }
+
+    public StudentDto updateStudent(String studentId, EditStudentCommand command) {
+        return new StudentDto();
+    }
+
+    public void updatePassword(String studentId, String password) {}
+
+    public void deleteStudent(String studentId) {}
+}
