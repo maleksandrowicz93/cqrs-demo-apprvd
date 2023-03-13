7b877ad6dcaaee4b8ac9eca5ccf654656eda5d42
maleksandrowicz93 <mateusz.aleksandrowicz93@gmail.com>
2023-03-13 09:12:51 +0100

    fix cors config

--- adapters/src/main/java/com/github/maleksandrowicz93/cqrsdemo/student/rest/controller/RestApiConfig.java
+++ adapters/src/main/java/com/github/maleksandrowicz93/cqrsdemo/student/rest/controller/RestApiConfig.java
@@ -3,6 +3,7 @@ package com.github.maleksandrowicz93.cqrsdemo.student.rest.controller;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -18,8 +19,12 @@ class RestApiConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry
-                        .addMapping("/student")
-                        .allowedOrigins(frontEnd);
+                        .addMapping("/student/**")
+                        .allowedOrigins(frontEnd)
+                        .allowedMethods(HttpMethod.GET.toString(),
+                                HttpMethod.POST.toString(),
+                                HttpMethod.PUT.toString(),
+                                HttpMethod.DELETE.toString());
             }
         };
     }
